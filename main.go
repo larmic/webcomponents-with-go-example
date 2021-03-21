fb07fae4ba075346d490730f635e40dd240d46b1
Lars Michaelis <mail@larmic.de>
2021-03-21 13:00:28 +0100

    feat: add http server and serve frontend

--- main.go
+++ main.go
@@ -1,9 +1,32 @@
 package main
 
 import (
+	"embed"
+	"io/fs"
 	"log"
+	"net/http"
+	"path"
 )
+
+const frontendFolder = "dist"
+
+//go:embed dist/*
+var static embed.FS
+
+// support http://localhost:8080 instead of http://localhost:8080/dist as app root
+// see https://github.com/golang/go/issues/43431#issuecomment-752662261
+type myFS struct {
+	content embed.FS
+}
+
+// support http://localhost:8080 instead of http://localhost:8080/dist as app root
+// see https://github.com/golang/go/issues/43431#issuecomment-752662261
+func (c myFS) Open(name string) (fs.File, error) {
+	return c.content.Open(path.Join(frontendFolder, name))
+}
 
 func main() {
 	log.Println("Hello webcomponents-with-go-example!")
+	http.Handle("/", http.FileServer(http.FS(myFS{static})))
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
