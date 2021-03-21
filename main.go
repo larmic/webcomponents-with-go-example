0d22da058943b7df6e10a5c806a243f24ad1cd7b
Lars Michaelis <mail@larmic.de>
2021-03-21 13:21:27 +0100

    feat: add info endpoint and use it in frontend (debug console) as an example

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 const frontendFolder = "dist"
 
 //go:embed dist/*
-var static embed.FS
+var frontend embed.FS
 
 // support http://localhost:8080 instead of http://localhost:8080/dist as app root
 // see https://github.com/golang/go/issues/43431#issuecomment-752662261
@@ -25,8 +25,13 @@ func (c myFS) Open(name string) (fs.File, error) {
 	return c.content.Open(path.Join(frontendFolder, name))
 }
 
+func info(w http.ResponseWriter, r *http.Request) {
+	_, _ = w.Write([]byte("{\"version\":\"0.0.1\"}"))
+}
+
 func main() {
 	log.Println("Hello webcomponents-with-go-example!")
-	http.Handle("/", http.FileServer(http.FS(myFS{static})))
+	http.Handle("/", http.FileServer(http.FS(myFS{frontend})))
+	http.HandleFunc("/info", info)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
