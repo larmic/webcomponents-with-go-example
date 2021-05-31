db0ac8c77e40a78ba88975098061e25cf61523df
Lars Michaelis <mail@larmic.de>
2021-05-31 18:54:00 +0200

    doc: use golang doc syntax

--- src/backend/stripEmbedContextPath.go
+++ src/backend/stripEmbedContextPath.go
@@ -6,14 +6,14 @@ import (
 	"path"
 )
 
-// support http://localhost:8080 instead of http://localhost:8080/dist as app root
+// StripEmbedContextPathFS support http://localhost:8080 instead of http://localhost:8080/dist as app root
 // see https://github.com/golang/go/issues/43431#issuecomment-752662261
 type StripEmbedContextPathFS struct {
 	Content     embed.FS
 	ContextPath string
 }
 
-// support http://localhost:8080 instead of http://localhost:8080/dist as app root
+// Open support http://localhost:8080 instead of http://localhost:8080/dist as app root
 // see https://github.com/golang/go/issues/43431#issuecomment-752662261
 func (c StripEmbedContextPathFS) Open(name string) (fs.File, error) {
 	return c.Content.Open(path.Join(c.ContextPath, name))
